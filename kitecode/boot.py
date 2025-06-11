26eb3d5da86721e2290539b7b4a9c21d521786d2
Chris Moeckel <mockel.chris@gmail.com>
2025-06-11 10:07:13 -0700

    Updates for 2025

--- kitecode/boot.py
+++ kitecode/boot.py
@@ -9,10 +9,11 @@ the pico starts up + , make the filesystem writeable by CircuitPython.
 import storage
 import board
 import digitalio
-
+    
 write_pin = digitalio.DigitalInOut(board.A0)
 write_pin.direction = digitalio.Direction.INPUT
 write_pin.pull = digitalio.Pull.UP
 
-if write_pin.value: # if tied to ground on startup, go into write mode
+# Setup sytem to be default write mode 
+if write_pin.value: # if tied to ground on startup, go into read mode to transfer data 
     storage.remount("/", readonly=False)
